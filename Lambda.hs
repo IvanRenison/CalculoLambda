899d9f93d959dcfe6ad65c43fe4248f0f60684c0
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2021-12-28 09:32:56 -0300

    Parser mejorado

--- Lambda.hs
+++ Lambda.hs
@@ -1,5 +1,9 @@
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE UnicodeSyntax #-}
 
-import Control.Applicative
+import Prelude.Unicode
+
+import Control.Applicative ( Alternative((<|>)) )
 import Data.Char
 import Data.Maybe
 
@@ -8,42 +12,49 @@ import Parser
 {-
 La gramatica del calculo lambda está definida por:
 
-V → x | (V)(V) | λx(V)
+V → x | (VV) | (λxV)
 x → ⟨Char⟩
 
 -}
 
 data LambdaExp =
     Var Char | App LambdaExp LambdaExp | Lambda Char LambdaExp
-    deriving (Show, Eq)
+    deriving (Eq)
 
+instance Show LambdaExp where
+    show (Var x) = [x]
+    show (App e1 e2) = "(" ++ show e1 ++ show e2 ++ ")"
+    show (Lambda x e) = "(λ" ++ [x] ++ show e ++ ")"
 
 parseVar :: Parser LambdaExp
-parseVar = Parser $ \s -> do
-    (v, xs) <- listToMaybe $ readsPrec 11 s
-    return (Var v, xs)
+parseVar = Parser $ \case
+    [] → Nothing
+    (x:xs) →
+        if isAlpha x ∧ x ≠ 'λ'
+            then Just (Var x, xs)
+            else Nothing
 
 parseApp :: Parser LambdaExp
-parseApp = Parser $ \s -> do
-    ('(':xs) <- return $ dropWhile isSpace s
-    (e1, ys) <- runParser parseLambdaExp $ dropWhile isSpace xs
-    (')':ys') <- return $ dropWhile isSpace ys
-    ('(':ys'') <- return $ dropWhile isSpace ys'
-    (e2, zs) <- runParser parseLambdaExp $ dropWhile isSpace ys''
-    (')':zs') <- return $ dropWhile isSpace zs
+parseApp = Parser $ \s → do
+    ('(':xs) ← return $ dropWhile isSpace s
+    (e1, ys) ← runParser parseLambdaExp $ dropWhile isSpace xs
+    (e2, zs) ← runParser parseLambdaExp $ dropWhile isSpace ys
+    (')':zs') ← return $ dropWhile isSpace zs
     return (App e1 e2, zs')
 
 
 parseLambda :: Parser LambdaExp
-parseLambda = Parser $ \s -> do
-    ('λ':xs) <- return $ dropWhile isSpace s
-    (Var x, ys) <- runParser parseVar $ dropWhile isSpace xs
-    ('(':ys') <- return $ dropWhile isSpace ys
-    (e, zs) <- runParser parseLambdaExp $ dropWhile isSpace ys'
-    (')':zs') <- return $ dropWhile isSpace zs
+parseLambda = Parser $ \s → do
+    ('(':xs) ← return $ dropWhile isSpace s
+    ('λ':xs') ← return $ dropWhile isSpace xs
+    (Var x, ys') ← runParser parseVar $ dropWhile isSpace xs'
+    (e, zs) ← runParser parseLambdaExp $ dropWhile isSpace ys'
+    (')':zs') ← return $ dropWhile isSpace zs
     return (Lambda x e, zs')
 
 parseLambdaExp :: Parser LambdaExp
 parseLambdaExp = parseApp <|> parseLambda <|> parseVar
 
+instance Read LambdaExp where
+    readsPrec _ = maybeToList . runParser parseLambdaExp
 
--- Parser.hs
+++ Parser.hs
@@ -1,44 +1,45 @@
 {-# LANGUAGE InstanceSigs #-}
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE UnicodeSyntax #-}
 
 module Parser where
 
 import Control.Applicative
 
-newtype Parser a = Parser { runParser :: String -> Maybe (a, String) }
+newtype Parser a = Parser { runParser :: String → Maybe (a, String) }
 
 instance Functor Parser where
-    fmap :: (a -> b) -> Parser a -> Parser b
-    fmap f (Parser p) = Parser $ \s -> do
-        (a, s') <- p s
+    fmap :: (a → b) → Parser a → Parser b
+    fmap f (Parser p) = Parser $ \s → do
+        (a, s') ← p s
         return (f a, s')
 
 instance Applicative Parser where
-    pure :: a -> Parser a
-    pure a = Parser $ \s -> return (a, s)
+    pure :: a → Parser a
+    pure a = Parser $ \s → return (a, s)
 
-    (<*>) :: Parser (a -> b) -> Parser a -> Parser b
-    (Parser f) <*> (Parser a) = Parser $ \s -> do
-        (f', s') <- f s
-        (a', s'') <- a s'
+    (<*>) :: Parser (a → b) → Parser a → Parser b
+    (Parser f) <*> (Parser a) = Parser $ \s → do
+        (f', s') ← f s
+        (a', s'') ← a s'
         return (f' a', s'')
 
 instance Monad Parser where
-    (>>=) :: Parser a -> (a -> Parser b) -> Parser b
-    (Parser p) >>= f = Parser $ \s -> do
-        (a, s') <- p s
+    (>>=) :: Parser a → (a → Parser b) → Parser b
+    (Parser p) >>= f = Parser $ \s → do
+        (a, s') ← p s
         runParser (f a) s'
 
 instance Alternative Parser where
     empty :: Parser a
     empty = Parser $ const Nothing
 
-    (<|>) :: Parser a -> Parser a -> Parser a
-    (Parser p) <|> (Parser q) = Parser $ \s -> p s <|> q s
+    (<|>) :: Parser a → Parser a → Parser a
+    (Parser p) <|> (Parser q) = Parser $ \s → p s <|> q s
 
 
-parseChar :: Char -> Parser Char
+parseChar :: Char → Parser Char
 parseChar c = Parser $ \case
-    [] -> Nothing
-    (x:xs) -> if x == c then Just (c, xs) else Nothing
+    [] → Nothing
+    (x:xs) → if x == c then Just (c, xs) else Nothing
 
