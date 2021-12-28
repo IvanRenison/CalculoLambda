493b08d0374faccc70be4c9b526603f6865a5d1b
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2021-12-28 10:37:43 -0300

    Reducción de λs

--- Lambda.hs
+++ Lambda.hs
@@ -2,9 +2,11 @@
 {-# LANGUAGE UnicodeSyntax #-}
 
 import Prelude.Unicode
+import Data.List.Unicode
 
 import Control.Applicative ( Alternative((<|>)) )
 import Data.Char
+import Data.List
 import Data.Maybe
 
 import Parser
@@ -57,4 +59,44 @@ parseLambdaExp = parseApp <|> parseLambda <|> parseVar
 
 instance Read LambdaExp where
     readsPrec _ = maybeToList . runParser parseLambdaExp
+
+
+
+
+variablesLibres :: LambdaExp → [Char]
+variablesLibres (Var x) = [x]
+variablesLibres (App e1 e2) = variablesLibres e1 ∪ variablesLibres e2
+variablesLibres (Lambda x e) = variablesLibres e \\ [x]
+
+remplazar :: LambdaExp → Char → LambdaExp → LambdaExp
+remplazar e x (Var y)
+    | y == x = e
+    | otherwise = Var y
+remplazar e x (App e1 e2) = App (remplazar e x e1) (remplazar e x e2)
+remplazar e x (Lambda y e')
+    | y == x = Lambda y e'
+    | y ∉ variablesLibres_e = Lambda y $ remplazar e x e'
+    | otherwise = Lambda y' $ remplazar e x $ remplazar (Var y') y e'
+    where
+        variablesLibres_e = variablesLibres e
+        varNoEn :: Char → [Char] → Char
+        varNoEn a [] = a
+        varNoEn a (b:bs)
+            | a == b = varNoEn a' bs
+            | otherwise = varNoEn a bs
+            where
+                a' :: Char
+                a'
+                    | isLetter (succ a) ∧ succ a ≠ 'λ' = succ a
+                    | otherwise = succ $ succ a
+        y' :: Char
+        y' = varNoEn y $ x:variablesLibres_e
+
+
+reducir :: LambdaExp → LambdaExp
+reducir (Var x) = Var x
+reducir (App (Lambda x e1) e2) = reducir $ remplazar e2 x e1
+reducir (App e1 e2) = reducir $ App (reducir e1) (reducir e2)
+reducir (Lambda x e) = Lambda x $ reducir e
+
 
